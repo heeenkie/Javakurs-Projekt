bf9d15bfee941cdf3e18a9fabfd6a7f32b472882
David Kruger <David.TropaKruger@gmail.com>
2019-11-15 16:01:50 +0100

    testdb

--- Project-WebApp/src/conf/sql/data.sql
+++ Project-WebApp/src/conf/sql/data.sql
@@ -8,3 +8,6 @@
  * Created: 2019-nov-15
  */
 
+INSERT INTO CUSTOMER VALUES(0001, 'David Tropa', 'Ekedalsvägen 12', 'Stockholm', 'Huddinge', 14145, 0707876582, 23454673);
+INSERT INTO CUSTOMER VALUES(0002, 'Marcus Küger', 'Horisontvägen 3', 'Stockholm', 'Sköndal', 84435, 0776524825, 63305703);
+INSERT INTO CUSTOMER VALUES(0003, 'Joao Barion', 'Efterblivnavägen 33', 'Göteborg', 'Slagsta', 24356, 0707876582, 43404673);
