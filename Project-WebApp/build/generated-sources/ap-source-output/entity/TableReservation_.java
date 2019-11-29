9b309f1dac05537cc0e22db6cbe4d844999f91fa
felicia <feas1700@student.miun.se>
2019-11-29 16:57:43 +0100

    ändring i css filen samt infogat lite bilder samt textttt

--- Project-WebApp/build/generated-sources/ap-source-output/entity/TableReservation_.java
+++ Project-WebApp/build/generated-sources/ap-source-output/entity/TableReservation_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-27T09:39:16")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-29T16:15:36")
 @StaticMetamodel(TableReservation.class)
 public class TableReservation_ { 
 
